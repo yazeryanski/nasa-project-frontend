5b9f79cb47bec18197cdd8c3b210747310973de2
Narek Yazeryan <yazeryann@gmail.com>
2023-02-06 14:33:35 +0400

    Adjusted frontend

--- src/pages/Launch.js
+++ src/pages/Launch.js
@@ -5,7 +5,7 @@ import Clickable from "../components/Clickable";
 const Launch = props => {
   const selectorBody = useMemo(() => {
     return props.planets?.map(planet => 
-      <option value={planet.kepler_name} key={planet.kepler_name}>{planet.kepler_name}</option>
+      <option value={planet.name} key={planet.name}>{planet.name}</option>
     );
   }, [props.planets]);
 
